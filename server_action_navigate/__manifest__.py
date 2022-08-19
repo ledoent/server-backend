6e79eaaf527b3ca5beb749e95bc07764b5f6efd5
Florent THOMAS <mailinglist@tdeo.fr>
2022-08-19 16:03:08 +0200

    Update __manifest__.py

--- server_action_navigate/__manifest__.py
+++ server_action_navigate/__manifest__.py
@@ -4,13 +4,13 @@
 
 {
     "name": "Server Actions - Navigate",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "GRAP, Odoo Community Association (OCA)",
     "summary": "Navigate between any items of any Odoo Models",
     "category": "Tools",
     "website": "https://github.com/OCA/server-backend",
     "license": "AGPL-3",
-    "depends": ["base", "base_automation"],
+    "depends": ["base_automation"],
     "maintainers": ["legalsylvain", "ashishhirpara"],
     "data": ["security/ir.model.access.csv", "views/view_ir_actions_server.xml"],
     "demo": ["demo/ir_actions_server.xml"],
